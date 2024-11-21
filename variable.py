24e863928888932d8d81326450a279df9db2088d
nktbashl <nb9374131@gmail.com>
2024-11-21 21:06:34 +0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = (number_of_hours_spent / homework )
+print("Курс:",course_name,"всего задач:",homework,'затрачено часов:',number_of_hours_spent, 'среднее время выполнения:',time_per_task  )
